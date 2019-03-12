9367adcca9857a176506eae4e1604ad964496b75
Frank <hawahe@163.com>
2019-03-12 17:27:31 +0800

    test

---
+++ practice/python book/alien_invasion/alien_invasion.py
@@ -0,0 +1,25 @@
+import sys
+
+import pygame
+
+def run_game():
+    #初始化游戏并创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    #设置背景色
+    bg_color = (230, 230, 230)
+    # 开始游戏的主循环
+    while True:
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #每次循环时都重绘屏幕
+        screen.fill(bg_color)
+
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
+run_game()
